e7ba35667b055f33cf7fd7ba25f2b2322579fa33
mike housey <mike.housey@gmail.com>
2021-03-18 12:10:09 -0500

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public List<Members> _lstMembers = new List<Members>();
+        public List<Claim> _lstClaims = new List<Claim>();
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetClaims(string ClaimDate)
+        {
+
+            var sdate = ClaimDate.Split('-');
+            var dt = DateTime.Parse(ClaimDate);//sdate[1] + '/' + sdate[2] + '/' + sdate[0];
+           
+
+
+            
+          
+            string path = Server.MapPath("\\App_Data\\Member.csv"); 
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+
+
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var record = new Members
+                    {
+
+                        MemberID = csv.GetField<int>("MemberID"),
+                        FirstName = csv.GetField<string>("FirstName"),
+                        LastName = csv.GetField<string>("LastName"),
+                        EnrollmentDate = csv.GetField<DateTime>("EnrollmentDate")
+                    };
+                    _lstMembers.Add(record);
+                }
+            }
+             path = Server.MapPath("\\App_Data\\Claim.csv");
+            using (var reader = new StreamReader(path))            
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var members = new List<Members>();
+                var claims = new List<Claim>();
+                csv.Read();
+                csv.ReadHeader();
+                while (csv.Read())
+                {
+                    var record = new Claim
+                    {
+
+                        MemberID = csv.GetField<int>("MemberID"),
+                        ClaimDate = csv.GetField<DateTime>("ClaimDate"),
+                        ClaimAmount = csv.GetField<decimal>("ClaimAmount")
+
+                    };
+                    _lstClaims.Add(record);
+                }
+            }
+            foreach(var m in _lstMembers)
+            {
+                m.lstClaims = _lstClaims.Where(x => x.MemberID == m.MemberID && x.ClaimDate <=dt).ToList();
+            }
+
+            return Json(_lstMembers, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+   
+}
---
+++ WebApplication2/Models/Classes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Claim {
+
+   public int MemberID { get; set; } 
+  
+	public DateTime ClaimDate { get; set; }
+	public decimal ClaimAmount { get; set;  }
+	public string FormatDate {get { return ClaimDate.ToShortDateString(); } }
+}
+
+public class Members
+{
+
+	public int MemberID { get; set; }
+
+	public DateTime EnrollmentDate { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public List<Claim> lstClaims { get; set; }
+	public string FormatDate { get { return EnrollmentDate.ToShortDateString(); } }
+}
+
